9935da1d6154205197cbdb95702a4b6a9aae8e0b
Brandon Davis <brancdavis@gmail.com>
2022-09-05 13:49:00 -0400

    bumped session timeout from 5 min to 10 min

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
-    cookie: {maxAge: 300000},
+    cookie: {maxAge: 600000},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
